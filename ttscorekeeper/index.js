e2aa7c2121b80faf1bb1d911b8cfcd0c2f5c37da
Jan Londén <jjlonden@gmail.com>
2019-02-10 12:46:19 +0100

    added toggle sound functionality

--- ttscorekeeper/index.js
+++ ttscorekeeper/index.js
@@ -4,23 +4,40 @@ const {
   and,
   append,
   both,
+  compose,
   cond,
   dropLast,
   either,
+  evolve,
   gte,
   identical,
+  identity,
+  ifElse,
   last,
   length,
   modulo,
+  not,
   nth,
   o,
   prop,
   sum,
+  when,
   __
 } = R
 
 const { Subject, fromEvent, operators } = rxjs
-const { startWith, scan, map, throttleTime, tap, shareReplay } = operators
+
+const {
+  startWith,
+  scan,
+  map,
+  throttleTime,
+  tap,
+  shareReplay,
+  pluck,
+  distinctUntilChanged
+} = operators
+
 const player1 = document.querySelector('.player-1')
 const player1Score = player1.querySelector('.score')
 const player1SetScore = player1.querySelector('.set-score')
@@ -31,6 +48,7 @@ const player2SetScore = player2.querySelector('.set-score')
 const player2Server = player2.querySelector('.server')
 const resetButton = document.querySelector('.reset-button')
 const undoButton = document.querySelector('.undo-button')
+const soundButton = document.querySelector('.sound-button')
 
 const announce = what => {
   const announcement = new SpeechSynthesisUtterance(what)
@@ -59,17 +77,14 @@ const shouldChangeServer = either(
   bothScoresAre10OrMore
 )
 
-const createNewState = (
-  player,
-  {
-    player1Score,
-    player1SetScore,
-    player2Score,
-    player2SetScore,
-    server,
-    setServer
-  }
-) => {
+const createGameState = ({ player }) => ({ sound }) => ({
+  player1Score,
+  player1SetScore,
+  player2Score,
+  player2SetScore,
+  server,
+  setServer
+}) => {
   const isPlayer1 = identical(player, 1)
   const newPlayer1Score = player1Score + 1
   const newPlayer2Score = player2Score + 1
@@ -92,21 +107,23 @@ const createNewState = (
   const someoneWon = and(someoneHas11OrMore(scores), someoneLeadsWith2)
   const player1Won = newPlayer1Score > newPlayer2Score
 
-  announce(
-    someoneWon
-      ? `${player1Won ? 'red' : 'blue'} wins the set ... ${
-        identical(newSetServer, 1) ? 'red' : 'blue'
-      } serves`
-      : `${
-        isPlayer1
-          ? identical(newServer, 1) ? newPlayer1Score : player2Score
-          : identical(newServer, 1) ? player1Score : newPlayer2Score
-      } ${
-        isPlayer1
-          ? identical(newServer, 1) ? player2Score : newPlayer1Score
-          : identical(newServer, 1) ? newPlayer2Score : player1Score
-      } ... ${identical(newServer, 1) ? 'red' : 'blue'} serves`
-  )
+  when(identity, () =>
+    announce(
+      someoneWon
+        ? `${player1Won ? 'red' : 'blue'} wins the set ... ${
+          identical(newSetServer, 1) ? 'red' : 'blue'
+        } serves`
+        : `${
+          isPlayer1
+            ? identical(newServer, 1) ? newPlayer1Score : player2Score
+            : identical(newServer, 1) ? player1Score : newPlayer2Score
+        } ${
+          isPlayer1
+            ? identical(newServer, 1) ? player2Score : newPlayer1Score
+            : identical(newServer, 1) ? newPlayer2Score : player1Score
+        } ... ${identical(newServer, 1) ? 'red' : 'blue'} serves`
+    )
+  )(sound)
 
   return {
     player1Score: someoneWon ? 0 : isPlayer1 ? newPlayer1Score : player1Score,
@@ -126,43 +143,82 @@ const createNewState = (
   }
 }
 
-const createInitialState = () => {
+const createInitialGameState = () => {
   const server = Math.floor(Math.random() * 2) + 1
 
-  return [
-    {
-      player1Score: 0,
-      player1SetScore: 0,
-      player2Score: 0,
-      player2SetScore: 0,
-      server,
-      setServer: server
-    }
-  ]
+  return {
+    player1Score: 0,
+    player1SetScore: 0,
+    player2Score: 0,
+    player2SetScore: 0,
+    server,
+    setServer: server
+  }
 }
 
 const store = storeSubject.pipe(
-  startWith(createInitialState()),
+  startWith({
+    game: [createInitialGameState()],
+    sound: true
+  }),
+
   throttleTime(250),
 
   scan((state, type) => {
     const newState = cond([
       [
         identical('player 1 scores'),
-        () => append(createNewState(1, last(state)), state)
+        () =>
+          evolve(
+            {
+              game: compose(
+                append,
+                createGameState({ player: 1 })(state),
+                last
+              )(prop('game', state))
+            },
+            state
+          )
       ],
 
       [
         identical('player 2 scores'),
-        () => append(createNewState(2, last(state)), state)
+        () =>
+          evolve(
+            {
+              game: compose(
+                append,
+                createGameState({ player: 2 })(state),
+                last
+              )(prop('game', state))
+            },
+            state
+          )
       ],
 
-      [identical('reset'), () => append(last(createInitialState()), state)],
+      [
+        identical('reset'),
+        () =>
+          evolve(
+            {
+              game: append(createInitialGameState())
+            },
+            state
+          )
+      ],
 
       [
         identical('undo'),
-        () => (length(state) >= 2 ? dropLast(1, state) : state)
-      ]
+        () =>
+          evolve(
+            {
+              game: ifElse(o(gte(__, 2), length), dropLast(1), identity)
+            },
+            state
+          )
+      ],
+
+      [identical('sound'), () => evolve({ sound: not }, state)]
     ])(type)
 
     console.log('\n')
@@ -172,37 +228,58 @@ const store = storeSubject.pipe(
       'color: #222; background-color: #fff0d3; font-weight: 700; padding: 0.5rem'
     )
 
-    console.table({ prevState: last(state), nextState: last(newState) })
+    console.log('%cprevState: ', 'color: #d13414')
+    console.log(state)
+    console.log('%cnextState: ', 'color: #1495d1')
+    console.log(newState)
 
     return newState
   }),
 
-  map(last),
   shareReplay(1)
 )
 
 // renderers
 
-store.subscribe(({ player1Score: score }) => (player1Score.innerHTML = score))
+const lastGameState = store.pipe(map(o(last, prop('game'))))
 
-store.subscribe(
-  ({ player1SetScore: score }) => (player1SetScore.innerHTML = score)
-)
+lastGameState
+  .pipe(pluck('player1Score'), distinctUntilChanged())
+  .subscribe(score => (player1Score.innerHTML = score))
 
-store.subscribe(({ player2Score: score }) => (player2Score.innerHTML = score))
+lastGameState
+  .pipe(pluck('player1SetScore'), distinctUntilChanged())
+  .subscribe(score => (player1SetScore.innerHTML = score))
 
-store.subscribe(
-  ({ player2SetScore: score }) => (player2SetScore.innerHTML = score)
-)
+lastGameState
+  .pipe(pluck('player2Score'), distinctUntilChanged())
+  .subscribe(score => (player2Score.innerHTML = score))
 
-store.subscribe(
-  ({ server }) =>
-    identical(1, server)
-      ? (player2Server.classList.remove('current'),
-        player1Server.classList.add('current'))
-      : (player1Server.classList.remove('current'),
-        player2Server.classList.add('current'))
-)
+lastGameState
+  .pipe(pluck('player2SetScore'), distinctUntilChanged())
+  .subscribe(score => (player2SetScore.innerHTML = score))
+
+lastGameState
+  .pipe(pluck('server'), distinctUntilChanged())
+
+  .subscribe(
+    server =>
+      identical(1, server)
+        ? (player2Server.classList.remove('current'),
+          player1Server.classList.add('current'))
+        : (player1Server.classList.remove('current'),
+          player2Server.classList.add('current'))
+  )
+
+store
+  .pipe(pluck('sound'), distinctUntilChanged())
+
+  .subscribe(
+    sound =>
+      sound
+        ? soundButton.classList.remove('muted')
+        : soundButton.classList.add('muted')
+  )
 
 // click listeners
 
@@ -216,6 +293,7 @@ fromEvent(player2, 'click').subscribe(() =>
 
 fromEvent(resetButton, 'click').subscribe(() => storeSubject.next('reset'))
 fromEvent(undoButton, 'click').subscribe(() => storeSubject.next('undo'))
+fromEvent(soundButton, 'click').subscribe(() => storeSubject.next('sound'))
 
 // pwa
 
